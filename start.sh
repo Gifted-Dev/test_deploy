677d968559cd8917c1f05df95fba7c3061f71ab8
Gifted <epopoola15@gmail.com>
2025-02-14 21:49:54 +0100

    set up start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT FastAPI.main:app
+# gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT FastAPI.main:app
+uvicorn FastAPI.main:app --host 0.0.0.0 --port 8000
+
 
